5068173ae4192401bbf499199f4bed498038c29f
henriquecesar139 <henriquepb139@gmail.com>
2022-07-30 21:09:11 -0300

    melhora esteticamente o formato de hora

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 function horario() {
-  hora = new Date()
-  horas = hora.getHours()
-  minutos = hora.getMinutes()
-  segundos = hora.getSeconds()
+  data = new Date()
+  horas = data.getHours()
+  minutos = data.getMinutes()
+  segundos = data.getSeconds()
+  dia = data.getDate()
+  mes = data.getMonth()
+  ano = data.getFullYear()
+
+  if (segundos < 10) {segundos = '0' + segundos}
+  if (minutos < 10) {minutos = '0' + minutos} 
+  if (horas < 10) {horas = '0' + horas} 
+  if (dia < 10) {dia = '0' + dia}
+  if (mes < 10) {mes = '0' + mes}
 
   document.getElementById("hora").innerText = horas + ": " + minutos + " : " + segundos;
 
-  document.getElementById("data").innerText = hora.getDate() + ' / ' + hora.getMonth()
+  document.getElementById("data").innerText = dia + ' / ' + mes + ' / ' + ano
 
 }
-
-
 
 timer = setInterval(horario, 1000)
