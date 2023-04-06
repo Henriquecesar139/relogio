1147543fb77fad3e6b95b5d27d505baf9b5a48c9
henriquecesar139 <henriquepb139@gmail.com>
2023-04-06 08:26:12 -0300

    resolve problema do mes

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ function horario() {
   mes = data.getMonth()
   ano = data.getFullYear()
 
+  mes = Number(mes) + 1
+
   if (segundos < 10) {segundos = '0' + segundos}
   if (minutos < 10) {minutos = '0' + minutos} 
   if (horas < 10) {horas = '0' + horas} 
